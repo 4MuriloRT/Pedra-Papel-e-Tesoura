cece1bf98b64aa887d954ed90fd8ec05ad62f263
Murilo Taborda <murilotaborda09@gmail.com>
2024-08-23 10:31:15 -0300

    Segundo commit

--- main.js
+++ main.js
@@ -31,3 +31,69 @@ const displayCompEscolha = (roboEscolha) =>{
             break;
     }
 }
+
+function playgame(){
+    if(pessoaPonto >= 10){
+        resultado.style.color = "green"
+        resultado.textContents = `Você Ganhou: ${pessoaPonto} x ${comPonto}`
+        compPonto = 0
+        pessoaPonto = 0
+        playerPontos.textContent = pessoaPonto
+        roboPontos.textContent = compPonto
+    }else if (compPonto >= 10){
+        resultado.style.color = "red"
+        resultado.textContents = `Você Perdeu: ${pessoaPonto} x ${comPonto}`
+        compPonto = 0
+        pessoaPonto = 0
+        playerPontos.textContent = pessoaPonto
+        roboPontos.textContent = compPonto
+    }
+}
+
+const playRound = (escolhas, compEscolha) => {
+    displayCompEscolha(compEscolha)
+    if(escolhas == compEscolha ){
+        resultado.textContent = 'Empate'
+    }else if(escolhas == "R" && compEscolha == "P"){
+        resultado.textContent = 'Papel enrola Pedra! Você perdeu'
+        compPonto++
+        roboPontos.textContent = compPonto
+    }else if(escolhas == "P" && compEscolha == "R"){
+        resultado.textContent = 'Papel enrola Pedra! Você ganhou'
+        pessoaPonto++
+        playerPontos.textContent = pessoaPonto
+    }else if(escolhas == "R" && compEscolha == "T"){
+        resultado.textContent = 'Pedra destroi Tesoura! Você ganhou'
+        pessoaPonto++
+        playerPontos.textContent = pessoaPonto
+    }else if(escolhas == "T" && compEscolha == "R"){
+        resultado.textContent = 'Pedra destroi Tesoura! Você perdeu'
+        compPonto++
+        roboPontos.textContent = compPonto
+    }else if(escolhas == "P" && compEscolha == "T"){
+        resultado.textContent = 'Tesoura corta Papel! Você perdeu'
+        compPonto++
+        roboPontos.textContent = compPonto
+    }else if(escolhas == "T" && compEscolha == "P"){
+        resultado.textContent = 'Tesoura corta Papel! Você ganhou'
+        pessoaPonto++
+        playerPontos.textContent = pessoaPonto
+    }
+    playgame()
+}
+
+optionsMenu.addEventListener("click", (e) =>{
+    const target = e.target
+    resultado.style.color = "black"
+    switch(target.className){
+        case "pedra":
+            playRound("R", getComputerEscolha())
+            break;
+        case "papel":
+            playRound("P", getComputerEscolha())
+            break;
+        case "tesoura":
+            playRound("T", getComputerEscolha())
+            break;
+    }
+})
